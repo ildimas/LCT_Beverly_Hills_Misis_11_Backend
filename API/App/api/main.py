6a96369401fd379dfe3ea8e64f8420729e894b04
dmitriy <dimailyushen@gmail.com>
2024-06-08 17:06:02 +0300

    structure fix

--- API/App/api/main.py
+++ API/App/api/main.py
@@ -1,5 +1,5 @@
 from fastapi import FastAPI
-from api.routes import auth
+from App.api.routes import auth
 
 app = FastAPI()
 app.mount(app=auth.auth_app, path="/auth")
---
+++ API/App/core/db.py
@@ -0,0 +1,13 @@
+from sqlalchemy import create_engine, Column, Integer, String
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
+
+SQLALCHEMY_DATABASE_URL = "sqlite:///./test.db"
+# The `connect_args` parameter is needed only for SQLite.
+engine = create_engine(
+    SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False}
+)
+
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
+Base = declarative_base()
